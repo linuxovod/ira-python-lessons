99922f0a0899a7578de2b06e543c5605b34cc285
linuxovod <michael.tabala@gmail.com>
2018-03-03 09:08:06 +0300

    продолжение второго урока

--- lesson 2/SquareSpiral1.py
+++ lesson 2/SquareSpiral1.py
@@ -1,6 +1,6 @@
 # SquaSpiral1.py - Рисование квадратной спирали бабабабабабабабабабабаба
 import turtle
 t = turtle.Pen()
-for x in range(100):
+for x in range(200):
     t.forward(x)
-    t.left(90)
+    t.left(91)
