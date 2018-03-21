1b4550890ea9682ac5ec9a5aa990fc8347d45df5
linuxovod <michael.tabala@gmail.com>
2018-03-21 20:16:25 +0300

    эксперименты с цветом

---
+++ lesson 2/SquareSpiral3.py
@@ -0,0 +1,7 @@
+# SquaSpiral1.py - Рисование квадратной спирали бабабабабабабабабабабаба
+import turtle
+t = turtle.Pen()
+t.pencolor("lemon chiffon")
+for x in range(100):
+    t.forward(x)
+    t.left(91)
